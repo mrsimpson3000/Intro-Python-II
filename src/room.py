4ae3a1394d1dcd26a0bffbd7a20ef1969cff0fe3
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-07-14 21:50:48 -0400

    Gave initial Room walls in all directions.

--- src/room.py
+++ src/room.py
@@ -5,3 +5,7 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.n_to = "wall"
+        self.e_to = "wall"
+        self.s_to = "wall"
+        self.w_to = "wall"
